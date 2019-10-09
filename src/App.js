7f5fba98f33ed2f3f322339d1d1180fce06d91de
Amaro Silva <arcamos.j@gmail.com>
2019-10-09 11:34:53 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -7,10 +7,21 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-      <iframe width="400" height="215" src="https://www.youtube.com/embed/XcU9GEUZTQA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="400" height="215" src="https://www.youtube.com/embed/XcU9GEUZTQA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+          </div>
+        </nav>
 
         <p>
-        React teste
+          React teste
         </p>
         <a
           className="App-link"
